3ee7758f03d16566c3c3ef7bf95a07443d8ac4ee
Rebelzob <karimdavid07@gmail.com>
2023-10-17 18:44:58 +0100

    Created book class and its methods

---
+++ app/models/book.rb
@@ -0,0 +1,17 @@
+require_relative 'item'
+
+class Book < Item
+  attr_reader :cover_state, :publisher, :id
+
+  def initialize(cover_state, publisher, *args)
+    super(*args)
+    @cover_state = cover_state
+    @publisher = publisher
+  end
+
+  private
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
