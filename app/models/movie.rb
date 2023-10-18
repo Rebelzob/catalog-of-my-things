017f8a2a075e32eaa2befdda6fb2490aed499f4b
diarisdiakite <diarisdiakite@gmail.com>
2023-10-18 03:33:46 +0200

    Update the Movie class iwith add_a_movie, list_all_movies and find_a_movie_by_index methods.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,9 +4,32 @@ class Movie < Item
   attr_reader :id
   attr_accessor :name, :genre, :author, :source, :label, :publish_date, :archived, :silent
 
+  include ItemData
+
+  @movies = []
+
   def initialize(name, genre, author, source, label, publish_date, archived, silent)
     super(name, genre, author, source, label, publish_date, archived)
     @silent = silent
+  end
+
+  def self.add_a_movie(movie_data)
+    movie_data = ItemData.collect_movie_data
+    movie_data[:author] = author
+    movie_data[:genre] = genre
+    movie_data[:label] = label
+    movie_data[:source] = source
+    movie = Movie.new(movie_data)
+
+    @movies << movie
+  end
+
+  def self.list_all_movies
+    @movies.each_with_index { |movie, index| puts "#{index} - #{movie.name}, #{movie.label}, ID:#{movie.id}" }
+  end
+
+  def self.find_movie_by_index(index)
+    @movies[index]
   end
 
   private
