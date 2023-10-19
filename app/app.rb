ce7bb95eef6ad7b021956d60cbfe58e122ff8f59
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-10-19 14:21:33 -0300

    Create a basic UI in order to run my code

--- app/app.rb
+++ app/app.rb
@@ -2,6 +2,10 @@ require_relative 'user_data/author_data'
 require_relative 'user_data/genre_data'
 require_relative 'user_data/label_data'
 require_relative 'models/game/options'
+require_relative 'models/game/save_load_game_author'
+require_relative 'models/game/game'
+require_relative 'models/game/author'
+
 # require_relative 'user_data/source_data'
 # require_relative 'user_data/item_data'
 
@@ -12,6 +16,7 @@ class App
   # include SourceData
   # include ItemData
   include Options
+  include LoadGameAuthor
 
   def initialize
     # collect_item_data
@@ -19,13 +24,52 @@ class App
     # author_data
     # genre_data
     # label_data
-    @games = []
-    @authors = []
+    @games = load_games
+    @authors = load_authors
+    display_menu
   end
+
+  def display_menu
+    loop do
+      option_list
+      @option = gets.chomp.to_i
+
+      run_app(@option)
+    end
+  end
+
+  def option_list
+    puts '1 - List all games'
+    puts '2 - List all authors'
+    puts '3 - Add a game'
+    puts '4 - Exit'
+  end
+
+  def run_app(option) 
+    case option
+    when 1
+      list_of_games
+    when 2 
+      list_of_authors
+    when 3 
+      add_game
+    when 4
+      handle_exit
+      sleep 1
+      exit
+    else
+    puts 'invalid option'
+    end
+  end   
+
+def handle_exit
+  save_authors
+  save_games
+  puts 'Thank you for using this app!'
+end  
 
   def init
     puts 'welcome'
-    list_of_games
-    list_of_authors
+    run_app
   end
 end
