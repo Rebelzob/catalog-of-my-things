4e0400b52524d28fd0b13f839a1acfdbf0ac18ae
Rebelzob <karimdavid07@gmail.com>
2023-10-19 20:48:53 +0100

    Added items to app

--- app/app.rb
+++ app/app.rb
@@ -6,6 +6,9 @@ require_relative 'models/game/save_load_game_author'
 require_relative 'models/game/game'
 require_relative 'models/game/author'
 require_relative 'models/display_methods'
+require_relative 'models/book/book_data'
+require_relative 'models/create_methods'
+require_relative 'models/display_methods'
 
 # require_relative 'user_data/source_data'
 
@@ -16,19 +19,18 @@ class App
   include GenreData
   include LabelData
   include Display
-  # include SourceData
+  include BookLabelData
   # include ItemData
+  include Create
   include Options
   include LoadGameAuthor
 
   def initialize
     # collect_item_data
     # source_data
-    # author_data
     # genre_data
-    # label_data
-    @labels = []
-    @books = []
+    @labels = load_labels
+    @books = load_books
     @games = load_games
     @authors = load_authors
     display_menu
@@ -45,9 +47,13 @@ class App
 
   def option_list
     puts '1 - List all games'
-    puts '2 - List all authors'
-    puts '3 - Add a game'
-    puts '4 - Exit'
+    puts '2 - List all books'
+    puts '3 - List all authors'
+    puts '4 - List all labels'
+    puts '5 - Add a label'
+    puts '6 - Add a game'
+    puts '7 - Add a book'
+    puts '8 - Exit'
   end
 
   def run_app(option)
@@ -55,10 +61,18 @@ class App
     when 1
       list_of_games
     when 2
-      list_of_authors
+      list_of_books
     when 3
-      add_game
+      list_of_authors
     when 4
+      list_of_labels
+    when 5
+      menu_create_label
+    when 6
+      add_game
+    when 7
+      menu_create_book
+    when 8
       handle_exit
       sleep 1
       exit
@@ -70,6 +84,8 @@ class App
   def handle_exit
     save_authors
     save_games
+    save_books
+    save_labels
     puts 'Thank you for using this app!'
   end
 
