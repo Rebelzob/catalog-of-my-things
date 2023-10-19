cfc60d3950a1753337daa0fdc762107834c197be
Rebelzob <karimdavid07@gmail.com>
2023-10-19 20:50:24 +0100

    added methods

--- app/models/book/book_data.rb
+++ app/models/book/book_data.rb
@@ -1,24 +1,37 @@
 require 'json'
-require_relative '../models/book'
 
-
-module BookData
+module BookLabelData
   def save_books(books)
-    File.open('database/books.json', 'w') do |file|
+    File.open('app/database/books.json', 'w') do |file|
       file.puts books.map { |book|
         {
-          id: book.id,
-          publisher: book.publisher,
-          publish_date: book.publish_date,
-          cover_state: book.cover_state,
+          'id' => book.id,
+          'publisher' => book.publisher,
+          'publish_date' => book.publish_date,
+          'cover_state' => book.cover_state,
         }
       }.to_json
     end
+    puts 'Books saved sucessfuly!'
+  end
+
+  def save_labels(labels)
+    File.open('app/database/labels.json', 'w') do |file|
+      file.puts labels.map { |label|
+        {
+          'id' => label.id,
+          'name' => label.name,
+        }
+      }.to_json
+    end
+    puts 'Labels saved sucessfuly!'
   end
 
   def load_books
+    return [] unless File.exist?('app/database/books.json')
+
     books = []
-    JSON.parse(File.read('database/books.json')).each do |book|
+    JSON.parse(File.read('app/database/books.json')).each do |book|
       books << Book.new(
         book['publisher'],
         book['publish_date'],
@@ -26,5 +39,23 @@ module BookData
       )
     end
     books
+
+  rescue StandardError => e
+    puts "Error loading Books: #{e.message}"
+  end
+
+  def load_labels
+    return [] unless File.exist?('app/database/labels.json')
+
+    labels = []
+    JSON.parse(File.read('app/database/labels.json')).each do |label|
+      labels << Label.new(
+        label['name'],
+      )
+    end
+    labels
+
+  rescue StandardError => e
+    puts "Error loading Books: #{e.message}"
   end
 end
